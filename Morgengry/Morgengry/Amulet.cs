ba151a41dfb5f2813b59607f736989d6dca55048
math964g <math964g@edu.eal.dk>
2018-10-24 09:20:36 +0200

    Did stuff

---
+++ Morgengry/Morgengry/Amulet.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Morgengry
+{
+    public enum Level { low, medium, high }
+    public class Amulet
+    {
+        public string ItemID;
+        public string Design;
+        public Level Quality;
+
+        public Amulet(string itemID)
+        {
+            ItemID = itemID;
+        }
+
+        public Amulet(string itemID, Level quality)
+        {
+            ItemID = itemID;
+            Quality = quality;
+        }
+
+        public Amulet(string itemID, Level quality, string design)
+        {
+            ItemID = itemID;
+            Design = design;
+            Quality = quality;
+        }
+
+        new
+        public string ToString()
+        {
+            return ItemID;
+        }
+    }
+}
---
+++ Morgengry/Morgengry/Book.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Morgengry
+{
+    public class Book
+    {
+        public string ItemID;
+        public string Title;
+        public double Price;
+
+        public Book(string itemID)
+        {
+            ItemID = itemID;
+        }
+
+        public Book(string itemID, string title)
+        {
+            ItemID = itemID;
+            Title = title;
+        }
+
+        public Book(string itemID, string title, double price)
+        {
+            ItemID = itemID;
+            Title = title;
+            Price = price;
+        }
+
+        new
+        public string ToString()
+        {
+            return ItemID;
+        }
+
+
+    }
+}
