b0f6a64a9faf8f5a6dfa322a15a06baff1f843f5
math964g <math964g@edu.eal.dk>
2018-10-29 10:39:34 +0100

    ADded methods and tests

---
+++ Morgengry/Morgengry/BookRepository.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Morgengry
+{
+    class BookRepository
+    {
+        List<Book> books;
+
+        public void AddBook(Book book)
+        {
+            books.Add(book);
+        }
+
+        public Book GetBook(string itemID)
+        {
+            foreach (Book book in books)
+            {
+                if (book.ItemID == itemID)
+                {
+                    return book;
+                }
+            }
+            return null;
+        }
+
+        public double GetTotalValue()
+        {
+            double value = 0;
+
+            foreach (Book book in books)
+            {
+                value += book.Price;
+            }
+            return value;
+        }
+    }
+}
